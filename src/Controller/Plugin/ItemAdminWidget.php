08edaf6db136c0fab60cb3a4fe902bcb4cd2ea34
rw0600 <rw0600@gmail.com>
2017-01-11 18:48:53 +0000

    Fix partial paths

--- src/Controller/Plugin/ItemAdminWidget.php
+++ src/Controller/Plugin/ItemAdminWidget.php
@@ -63,7 +63,7 @@ class ItemAdminWidget extends AbstractPlugin
             'allowDelete' => $this->accountService->isAllowed('item', 'delete'),
         ]);
 
-        return $viewModel->setTemplate('cms-item-module/item/admin-widget.phtml');
+        return $viewModel->setTemplate('boxspaced/cms-item-module/item/admin-widget.phtml');
     }
 
     /**
